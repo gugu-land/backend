b534ea02b0ad93c358a7f3a9c6e0d2085c068006
JOY0987 <jjoyewon@gmail.com>
2023-07-18 16:06:54 +0900

    initial commit

---
+++ src/main/java/shop/stylehub/stylehub/StylehubApplication.java
@@ -0,0 +1,13 @@
+package shop.stylehub.stylehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/stylehub/stylehub/StylehubApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.stylehub.stylehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
